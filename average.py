3df2ca39e5f2eb33631fd2aba9aec8d91d6b72cb
Varioun <77806381+Varioun@users.noreply.github.com>
2022-01-31 12:56:09 -0500

    Add files via upload

---
+++ average.py
@@ -0,0 +1,13 @@
+first = input("Input a number: ")
+second = input("Next: ")
+third = input("Last: ")
+
+print(first + " " + second + " " + third)
+
+first = float(first)
+second = float(second)
+third = float(third)
+
+average = (first + second + third) / 3
+
+print(average)
